bf3312fd611e0c33882d3ebaa332d182d99b502d
Thejbm19 <jaumebauza10@gmail.com>
2021-10-05 12:57:25 +0200

    scripts

---
+++ Tema2/Assets/scripts/MyFirstScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+
+
+public class MyFirstScript : MonoBehaviour
+{
+    public int playerAge = 35;
+    private float playerSpeed = 5.25f;
+    public string playerName = "Jaume";
+    [SerializeField] private bool gameOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
