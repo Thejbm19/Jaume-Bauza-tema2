c5cb0fdc1eeb9a41212f4d98fe3bd8726d847780
Thejbm19 <jaumebauza10@gmail.com>
2021-10-18 11:13:27 +0200

    continuacio classe

--- Tema2/Assets/scripts/MyFirstScript.cs
+++ Tema2/Assets/scripts/MyFirstScript.cs
@@ -6,24 +6,53 @@ using UnityEngine;
 public class MyFirstScript : MonoBehaviour
 {
     /*
-    public int playerAge = 35;
+    
     private float playerSpeed = 5.25f;
     public string playerName = "Jaume";
-    [SerializeField] private bool gameOver;*/
+    [SerializeField] private bool gameOver;
 
     public int x = 5;
     public int y = 2;
 
     public float a = 5f;
     public float b = 2f;
+    
+    public bool isRaining = false;
+    */
+    
+    public int playerAge = 35;
 
     // Start is called before the first frame update
     void Start()
     {
+       /*
         Debug.Log($"Suma: {x + y}");
         Debug.Log("Resta: " + (x - y));
         Debug.Log($"Divide: { x / y}");
         Debug.Log($"Multiplica: {x * y}");
+        Debug.Log(string.Format("Divide: {0} / {1} = {2}", x, y, x / y));
+       
+
+
+        if (isRaining == true) 
+        {
+            Debug.Log("Lleva paraguas");
+        } else
+        {
+            Debug.Log("No llueve. No lleves paraguas");
+        }*/
+
+        if(playerAge >= 18)
+        {
+            Debug.Log("Eres mayor de edad");
+        }else if (playerAge >= 13)
+        {
+            Debug.Log("Eres adolescente");
+        }
+        else
+        {
+            Debug.Log("Eres un niño");
+        }
     }
 
     // Update is called once per frame
--- Tema2/Assets/scripts/Vehicle.cs
+++ Tema2/Assets/scripts/Vehicle.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class Vehicle : MonoBehaviour
 {
     //Variable que guarda el sonido del vehiculo
-    [SerializeField]private string sound = "¡Brum,brum!"; 
-    
+    [SerializeField]private string sound = "¡Brum,brum!";
+    //Variable que gurada el nombre del GameObject
+    [SerializeField]private string name = "";
+    //Variable que guarda el numero de ruedas del GameObject o coche
+    [SerializeField]private int numberWheels = 4;
+    //Variable que guarda si la ambulancia tiene o no sirena
+    [SerializeField] private bool hasSiren = false;
 
+    [SerializeField] private bool isOn = true;
 
 
 
@@ -17,8 +23,29 @@ public class Vehicle : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   /*
+        Debug.Log($"{name} tiene {numberWheels} ruedas y hace {sound}");
+
+        if (hasSiren == true)
+        {
+            Debug.Log($"{name} tiene sirena");
+        }  else
+        {
+            Debug.Log($"{name} no tiene sirena");
+        }*/
+
+        if (isOn == true)
+        {
+            Debug.Log($"{sound}");
+        }
+        else if (hasSiren == true)
+        {
+            Debug.Log($"{name} no esta en marcha");
+        }
+        else
+        {
+            Debug.Log($"{name} hara {sound} cuando se ponga en marcha");
+        }
     }
 
     // Update is called once per frame
